fa2d0389029db09f26d8a575555579d3ea651992
saadalishah <sashsaad@gmail.com>
2018-10-18 04:08:14 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  odd=[1,3,5];
+  even=[2,4,6];
+   toggle= true;
+
+  onToggle() {
+    this.toggle = !this.toggle;
+  }
+
+
+}
